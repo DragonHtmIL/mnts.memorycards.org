c60662f2fe286be506bad1e41e585feaa4227468
ArthurVesta <meirfons1@gmail.com>
2026-05-07 02:58:13 +0300

    Delete index_data directory

--- index_data/scripts/window_load.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('load', function() {
-  languagesContents();
-  setInterval(displayTime, 1);
-  iconAllLoad();
-  autoLanguageLoadNew();
-  initializeGame();
-  deviceTypeElementsRegistration();
-});
